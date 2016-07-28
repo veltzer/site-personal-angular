2aea7aa6b33d698cd6656eb1fd03945bb5f24a0b
Mark Veltzer <mark.veltzer@gmail.com>
2016-07-28 22:50:09 +0300

    Updates

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
 (function() {
 	'use strict';
-	angular.module('MyApp',['ngMaterial']);
+	var app=angular.module('MyApp',['ngMaterial']);
+	app.controller('AppCtrl', function($scope) {
+		$scope.projects=[
+			{
+				name: 'demos-linux'
+			},
+			{
+				name: 'fastlog'
+			},
+			{
+				name: 'jschess'
+			},
+			{
+				name: 'openbook'
+			},
+			{
+				name: 'riddling'
+			},
+			{
+				name: 'keynote'
+			},
+			{
+				name: 'slides'
+			},
+			{
+				name: 'templar'
+			},
+			{
+				name: 'pdmt'
+			},
+			{
+				name: 'scheduler'
+			}
+		];
+	});
 })();
