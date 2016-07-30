4ae46e6de674f6a20b89f40e791b61098afd6375
Mark Veltzer <mark.veltzer@gmail.com>
2016-07-30 12:31:23 +0300

    Updates

--- index.js
+++ index.js
@@ -2,49 +2,10 @@
 	'use strict';
 	var app=angular.module('MyApp',['ngMaterial']);
 	app.controller('AppCtrl', function($scope) {
-		$scope.projects=[
-			{
-				name: 'demos-linux',
-				description: 'this is description'
-			},
-			{
-				name: 'fastlog',
-				description: 'this is description'
-			},
-			{
-				name: 'jschess',
-				description: 'this is description'
-			},
-			{
-				name: 'openbook',
-				description: 'this is description'
-			},
-			/*
-			{
-				name: 'riddling',
-				description: 'this is description'
-			},
-			{
-				name: 'keynote',
-				description: 'this is description'
-			},
-			{
-				name: 'slides',
-				description: 'this is description'
-			},
-			{
-				name: 'templar',
-				description: 'this is description'
-			},
-			{
-				name: 'pdmt',
-				description: 'this is description'
-			},
-			{
-				name: 'scheduler',
-				description: 'this is description'
-			}
-			*/
-		];
+		const gh = new GitHub();
+		const me = gh.getUser('veltzer');
+		me.listRepos(function(err, repos) {
+			$scope.projects=repos;
+		});
 	});
 })();
