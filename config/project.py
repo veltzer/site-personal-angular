b74ad1016ac41a8185f45ae90d4ff6ea332fdf2f
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:51:13 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,13 +1,6 @@
-project_github_username = "veltzer"
-project_name="veltzer.github.io"
-project_long_description="Mark Veltzers web site at github"
-project_short_description=project_long_description
-project_year_started=2014
-project_description="This is my website at github showing some of the results for the various projects"
-project_website="https://veltzer.github.io/openbook"
-# keywords to put on html pages or for search, dont put the name of the project or my details
-# as they will be added automatically...
-project_keywords=[
+description_short="Mark Veltzers web site at github"
+description_long="This is my website at github showing some of the results for the various projects"
+keywords=[
     "software",
     "git",
     "open source",
@@ -15,6 +8,3 @@ project_keywords=[
     "Mark Veltzer",
     "GNU",
 ]
-project_paypal_donate_button_id="E3RJEDGT3T32N"
-project_google_analytics_tracking_id="UA-80925041-1"
-codacy_id = None
