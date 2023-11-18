a5ca04b7ad144ccaa40e153faf8b7de197b22185
Mark Veltzer <mark.veltzer@gmail.com>
2023-11-18 12:12:55 +0200

    meta data change

--- config/project.py
+++ config/project.py
@@ -1,5 +1,5 @@
 name = "site-personal-angular"
-description_short="Mark Veltzers web site at github"
+description_short="My personal site written in angular"
 description_long="This is my website at github showing some of the results for the various projects"
 keywords=[
     "software",
