431f6f06f4347474c26b63d0482dd762cedfa443
Mark Veltzer <mark.veltzer@gmail.com>
2026-08-20 01:40:01 +0300

    no commit message given

---
+++ config/project.lua
@@ -0,0 +1,4 @@
+NAME = "site-personal-angular"
+DESCRIPTION_SHORT = "My personal site written in angular"
+DESCRIPTION_LONG = "This is my website at github showing some of the results for the various projects"
+KEYWORDS = {"software", "git", "open-source", "free-source", "gnu"}
